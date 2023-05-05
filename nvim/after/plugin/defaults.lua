9f648d1d61afcc06792e734025d6a48a2d68d54d
Giuliano Machado <giufma@gmail.com>
2023-05-05 06:10:30 -0300

    fix indentation to use spaces

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -1,8 +1,25 @@
 -- CUSTOM SETTINGS
 -- Tab/Indentation settings
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+-- tabstop
+-- The width of a hard tabstop measured in "spaces" -- effectively the (maximum) width of an actual tab character.
+-- set something weird so it is not masqueraded
+vim.opt.tabstop = 8
+
+-- softtabstop
+-- Setting this to a non-zero value other than tabstop will make the tab key (in insert mode) insert a combination of spaces (and possibly tabs) to simulate tab stops at this width.
+vim.opt.softtabstop = 0
+
+-- shiftwidth
+-- The size of an "indent". It's also measured in spaces, so if your code base indents with tab characters then you want shiftwidth to equal the number of tab characters times tabstop. This is also used by things like the =, > and < commands.
 vim.opt.shiftwidth = 2
+
+-- expandtab
+-- Enabling this will make the tab key (in insert mode) insert spaces instead of tab characters. This also affects the behavior of the retab command.
+vim.opt.expandtab = true
+
+-- smarttab
+-- Enabling this will make the tab key (in insert mode) insert spaces or tabs to go to the next indent of the next tabstop when the cursor is at the beginning of a line (i.e. the only preceding characters are whitespace).
+vim.opt.smarttab = true
 
 vim.opt.incsearch = true
 
